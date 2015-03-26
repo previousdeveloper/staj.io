e02842c8e4467fff252e1949db222df079187050
gokhanKaradas <gokhan.karadas1992@gmail.com>
2015-03-27 00:05:31 +0200

    Added mongoose schema for company.

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ var mongoose = require('mongoose');
 var morgan = require('morgan');
 var config = require('./app/config/dbconfig');
 var winston = require('winston');
-
+var bodyParser = require('body-parser');
 
 var app = express();
 
 winston.add(winston.transports.File, {filename: 'loggerfile.log'});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 
 var winston = require('winston');
 
@@ -17,10 +20,6 @@ mongoose.connect(config.app.dbURL, {server: {keepAlive: 1, auto_reconnect: true}
     }
 });
 
-
-
-
-
-// Start the server
+require('./app/controller/companyctrl')(app);
 
 module.exports = app;
